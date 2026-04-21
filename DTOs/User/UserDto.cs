19f8ef7c0f0e957ab7a2598e67f7a4991d203a0a
NemanjaStankovic <nemanjast@elfak.rs>
2026-04-21 20:48:13 +0200

    UserDTOs added

---
+++ DTOs/User/UserDto.cs
@@ -0,0 +1,11 @@
+public class UserDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    public UserRole Role { get; set; }
+
+    public bool IsActive { get; set; }
+}
