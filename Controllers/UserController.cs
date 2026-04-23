093c3564dc4222acd4257ae2de6b52f6d1e6ccde
NemanjaStankovic <nemanjast@elfak.com>
2026-04-23 16:32:44 +0200

    endpoint to add user and get it by Id added

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/users")]
 public class UserController : ControllerBase
 {
     private readonly GarageDbContext Context;
@@ -11,12 +11,30 @@ public class UserController : ControllerBase
     {
         Context = context;
     }
-    [Route("addUser")]
+    [Route("user")]
     [HttpPost]
-    public async Task<ActionResult<UserDto>> addUser(CreateUserDto dto)
+    public async Task<ActionResult<UserDto>> CreateUser(CreateUserDto dto)
     {
         if (string.IsNullOrWhiteSpace(dto.Email))
             return BadRequest("Email is required");
+        if (string.IsNullOrWhiteSpace(dto.Password))
+            return BadRequest("Password is required");
+        var exists = await Context.Users.AnyAsync(u => u.Email == dto.Email);
+        if (exists)
+            return BadRequest("User already exists");
+
+        if (dto.Password.Length < 8)
+            return BadRequest("Password must be at least 8 characters long");
+
+        if (!dto.Password.Any(char.IsUpper))
+            return BadRequest("Password must contain at least one uppercase letter");
+
+        if (!dto.Password.Any(char.IsDigit))
+            return BadRequest("Password must contain at least one number");
+
+        if (!dto.Password.Any(ch => !char.IsLetterOrDigit(ch)))
+            return BadRequest("Password must contain at least one special character");
+
         var user = new User
         {
             Email = dto.Email,
@@ -26,6 +44,22 @@ public class UserController : ControllerBase
         };
         Context.Users.Add(user);
         await Context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            Role = user.Role,
+            IsActive = user.IsActive
+        });
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDto>> GetUserById(int id)
+    {
+        var user = await Context.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+        if (user == null)
+            return NotFound();
 
         return Ok(new UserDto
         {
--- DTOs/User/UserDto.cs
+++ DTOs/User/UserDto.cs
@@ -2,8 +2,6 @@ public class UserDto
 {
     public int Id { get; set; }
     public string Email { get; set; }
-    public string PasswordHash { get; set; }
-
     public UserRole Role { get; set; }
 
     public bool IsActive { get; set; }
