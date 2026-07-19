47254e63af39d776f4e6d5646e9354c59f9b9460
NemanjaStankovic <nemanjast@elfak.com>
2026-07-19 18:24:00 +0200

    Add automatic database migration with EF migrations on startup

--- Program.cs
+++ Program.cs
@@ -41,6 +41,12 @@ builder.Services
 builder.Services.AddScoped<AuthService>();
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<GarageDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseAuthentication();
 app.UseAuthorization();
 
